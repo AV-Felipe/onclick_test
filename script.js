05937a197af7d90f92cda071b4ca9c8976de31b1
AV-Felipe <aerogrado@gmail.com>
2021-11-23 23:11:29 -0300

    Implement rate variable

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
 const submitButton = document.getElementById('sendButton');
 const nameInputField = document.getElementById('nameField');
 const mailInputField = document.getElementById('mailField');
-const comentInputField = document.getElementById('messageField');
+const commentInputField = document.getElementById('messageField');
 const inputForm = document.getElementById('feedbackForm');
 const clientsReviewList = document.getElementById('reviewList');
+let clientRate = 0;
+
+const pizzaRateImage = document.querySelectorAll('.pizzaPieImg');
+
+//IMAGE ANIMATION
+console.log(pizzaRateImage);
+
+[...pizzaRateImage].map(pizza => {
+    pizza.addEventListener('mouseover', ()=>{
+        pizza.src = "assets/images/pizza-pie-yes.png";
+    })
+    pizza.addEventListener('mouseout', pizzaChange);
+});
+
+function pizzaChange(){
+    this.src="assets/images/pizza-pie-no.png";
+}
+
+pizzaRateImage[0].onclick = () =>{
+    clientRate = 1;
+    console.log(clientRate);
+}
+pizzaRateImage[1].onclick = () =>{
+    clientRate = 2;
+    console.log(clientRate);
+}
+pizzaRateImage[2].onclick = () =>{
+    clientRate = 3;
+    console.log(clientRate);
+}
+pizzaRateImage[3].onclick = () =>{
+    clientRate = 4;
+    console.log(clientRate);
+}
+pizzaRateImage[4].onclick = () =>{
+    clientRate = 5;
+    console.log(clientRate);
+}
 
 
+//FORM SUBMISSION
 submitButton.addEventListener('click', submitForm);
 
 function submitForm(){
     
     //fields validation
-    if (nameInputField.value == "" || mailInputField.value == "" || comentInputField.value == ""){
+    if (nameInputField.value == "" || mailInputField.value == "" || commentInputField.value == ""){
         if (document.contains(document.getElementById('warningMessage'))) {
             return;
         }else{
@@ -31,20 +70,26 @@ function submitForm(){
         document.getElementById('warningMessage').remove();
     }
 
+    //review posting
     clientsReviewList.innerHTML += `
     <li class="clientReview">
                     <p class="reviewerName">${nameInputField.value}</p>
                     <p class="reviewerMail">${mailInputField.value}</p>
                     <p class="reviewerText">
-                        ${comentInputField.value}
+                        ${commentInputField.value}
                     </p>
                     <p class="reviewerScore">
                         <img src="assets/images/pizza-pie-yes.png" alt="nota" />
-                        5
+                        ${clientRate.toString()}
                     </p>
                 </li>
-    `
+    `;
 
+    nameInputField.value = "";
+    mailInputField.value = "";
+    commentInputField.value = "";
+    clientRate = 0;
+    
     console.log(nameInputField.value);
     console.log(inputForm);
 }
